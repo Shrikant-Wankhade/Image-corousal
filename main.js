39e60c7185a8dd2e9606d46139830018f90a763c
Shrikant-Wankhade <wshrikantp10@gmail.com>
2022-12-05 21:13:25 +0530

    fixed error -fixed the error related to getElementById -refactored the code

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 let images = [
-    "/images/wrestling.webp",
-    "/images/cricket.webp",
-    "/images/cycling.webp",
-    "/images/f1.webp",
-    "/images/football.webp",
-    "/images/nfl.webp",
-    "/images/tennis.webp"
+    "./images/wrestling.webp",
+    "./images/cricket.webp",
+    "./images/cycling.webp",
+    "./images/f1.webp",
+    "./images/football.webp",
+    "./images/nfl.webp",
+    "./images/tennis.webp"
 ];
 
 let currentIndex = 0;
 
 //variables
-let prevButton = document.getElementById('#prevButton');
-let nextButton = document.getElementById('#nextButton');
-var img = document.querySelector('img');
-let back = document.getElementsByClassName('.imageCorousal');
+let prevButton = document.getElementById('prevButton');
+let nextButton = document.getElementById('nextButton');
+let img = document.getElementById('img');
 
 
 //event listeners
@@ -30,8 +29,7 @@ function nextImage(){
     if(currentIndex == images.length){
         currentIndex = 0;
     }
-    img.setAttribute('src',images[currentIndex]);
-    back.style.color = "white";
+    img.src = images[currentIndex];
 }
 
 
@@ -40,5 +38,5 @@ function prevImage(){
     if(currentIndex < 0){
         currentIndex = images.length-1;
     }
-    img.setAttribute('src',images[currentIndex]);
+    img.src = images[currentIndex];
 }
